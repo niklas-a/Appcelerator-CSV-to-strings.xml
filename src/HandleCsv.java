ed11bb2be5e7904b1b526cdc8cb57ea8706e1035
Niklas Agevik <niklas.agevik@gmail.com>
2012-01-10 12:48:10 +0100

    tst

--- src/HandleCsv.java
+++ src/HandleCsv.java
@@ -81,7 +81,7 @@ public class HandleCsv {
 			System.out.println("Reading " + InputFile + " " + inputEncoding);
 			String strLine = br.readLine();
 			String[] languages = strLine.split(";");
-			System.out.println(strLine);
+
 			br.mark(60000);
 			for (int i = 1; i < languages.length; i++) {
 				String filename = languages[i] + ".xml";
